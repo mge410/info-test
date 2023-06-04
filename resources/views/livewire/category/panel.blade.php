73155410f484e01cd11694dd504320146a6bde13
Vladimir <v0v.voron2005@yandex.ru>
2023-06-04 14:06:28 +0300

    Add bootstrap-icons for CRUD

--- resources/views/livewire/category/panel.blade.php
+++ resources/views/livewire/category/panel.blade.php
@@ -4,20 +4,33 @@
     <ul class="list-unstyled ps-0">
         @foreach($categories as $category)
             <li class="mb-1">
-                <button wire:click.defer="loadProducts({{ $category->id }})" class="btn btn-toggle align-items-center rounded collapsed"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#{{ $category->id }}-collapse"
-                        aria-expanded="false">
-                    Category {{ $category->title }}
-                    ({{ $category->subcategory_count }})
-                </button>
-                <span></span>
+                <div class="row align-items-center">
+                    <button wire:click.defer="loadProducts({{ $category->id }})" class="btn btn-toggle align-items-center rounded collapsed col-6"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#{{ $category->id }}-collapse"
+                            aria-expanded="false">
+                        Category {{ $category->title }}
+                        ({{ $category->subcategory_count }})
+                    </button>
+                    <div class="col-5">
+                        <i class="bi link   bi-plus-lg text-success m-1"></i>
+                        <i class="bi link   bi-pencil-fill text-primary m-1"></i>
+                        <i class="bi link   bi-trash-fill text-danger m-1"></i>
+                    </div>
+                </div>
                 <div class="collapse"
                      id="{{ $category->id }}-collapse" style="">
                     <ul class="btn-toggle-nav list-group list-unstyled fw-normal pb-1 small">
                         @foreach($category->subcategory as $subcategory)
                             <li class="list-group-item" wire:click="loadProducts({{ $subcategory->id }})">
-                                <a href="#">{{ $subcategory->title }}</a>
+                                <div class="row align-items-center">
+                                    <a class="d-block col-6" href="#">{{ $subcategory->title }}</a>
+                                    <div class="col-5">
+                                        <i class="bi link bi-plus-lg text-secondary m-1"></i>
+                                        <i class="bi link bi-pencil-fill text-secondary m-1"></i>
+                                        <i class="bi link bi-trash-fill text-secondary m-1"></i>
+                                    </div>
+                                </div>
                             </li>
                         @endforeach
                     </ul>
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
         }
     },
 });
